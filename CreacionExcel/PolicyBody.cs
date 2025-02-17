b95f89136f990c2cc4197b7134472cb4dd708288
Alan Chávez <alan.chavez@techsoft.com.mx>
2025-02-17 16:41:12 -0700

    Se modifico servicio para crear excel

--- CreacionExcel/PolicyBody.cs
+++ CreacionExcel/PolicyBody.cs
@@ -7,14 +7,12 @@
         public string ClaseDocumento { get; set; } = string.Empty;
         public DateTime FechaContable { get; set; }//yyyy-MM-dd
         public DateTime Periodo { get; set; }//MM
-        public int? TipoCambio { get; } = null;
+        public string TipoCambio { get; } = "NULL";
         public string IdMoneda { get; } = "MXM";
         public int ReferenciaCabecero { get; set; }//Pendiente
         public string TextoCabecero { get; set; } = string.Empty;//Pendiente
-        public string LlaveSistema { get; set; } = string.Empty;//Confirmar
-        public string PrimerNumeroReferencia { get; set; } = string.Empty;//Confirmar
-
-        //Detalle
+        public int LlaveSistema { get; set; }
+        public int PrimerNumeroReferencia { get; set; }
         public int NumeroPosicion { get; set; }
         public int ClaveContable { get; set; }
         public int NumeroCuenta { get; set; }
@@ -22,7 +20,7 @@
         public string CentroCostosId { get; set; } = string.Empty;
         public string AsignacionId { get; set; } = string.Empty;
         public string Texto { get; set; } = string.Empty;//PENDIENTE
-        public int IndicadorIva { get; set; }
+        public string IndicadorIva { get; set; } = string.Empty;
         public decimal? ImporteImpuesto { get; set; }
         public string DivisionId { get; set; } = string.Empty;
     }
--- CreacionExcel/Program.cs
+++ CreacionExcel/Program.cs
@@ -24,8 +24,8 @@ namespace CreacionExcel
                     Periodo = DateTime.Now,
                     ReferenciaCabecero = 1,
                     TextoCabecero = "TextoCabecero",
-                    LlaveSistema = "LlaveSistema",
-                    PrimerNumeroReferencia = "PrimerNumeroReferencia",
+                    LlaveSistema = 123456789,
+                    PrimerNumeroReferencia = 123456789,
                     NumeroPosicion = 1,
                     ClaveContable = 1,
                     NumeroCuenta = 1,
@@ -33,7 +33,7 @@ namespace CreacionExcel
                     CentroCostosId = "CentroCostosId",
                     AsignacionId = "AsignacionId",
                     Texto = "Texto",
-                    IndicadorIva = 1,
+                    IndicadorIva = "asdfghjklñ",
                     ImporteImpuesto = 1,
                     DivisionId = "DivisionId"
                 });
@@ -42,62 +42,92 @@ namespace CreacionExcel
         }
         static void GenerarExcel(List<PolicyBody> listadoPoliza)
         {
-            using (var workbook = new XLWorkbook())
+            #region Generacion Primer excel
+            var woorbook1 = new XLWorkbook();
+            var worksheet1 = woorbook1.Worksheets.Add("Polizas");
+            worksheet1.Cell("A1").Value = "folioPoliza";
+            worksheet1.Cell("B1").Value = "tipo";
+            worksheet1.Cell("C1").Value = "fechaPoliza";
+            worksheet1.Cell("D1").Value = "importePoliza";
+            worksheet1.Cell("E1").Value = "concepto";
+            worksheet1.Cell("F1").Value = "renglon";
+            worksheet1.Cell("G1").Value = "codigoCuentaContable";
+            worksheet1.Cell("H1").Value = "concepto2";
+            worksheet1.Cell("I1").Value = "cargo";
+            worksheet1.Cell("J1").Value = "abono";
+            worksheet1.Cell("K1").Value = "Nombre Cuenta Contable";
+            #endregion
+
+            #region Generacion Segundo Excel
+            var workbook2 = new XLWorkbook();
+            var worksheet2 = workbook2.Worksheets.Add("Polizas");
+            worksheet2.Cell("A1").Value = "fechaDocumento";
+            worksheet2.Cell("B1").Value = "ddSociedad";
+            worksheet2.Cell("C1").Value = "claseDocumento";
+            worksheet2.Cell("D1").Value = "fechaContable";
+            worksheet2.Cell("E1").Value = "periodo";
+            worksheet2.Cell("F1").Value = "tipoCambio";
+            worksheet2.Cell("G1").Value = "codigoMoneda";
+            worksheet2.Cell("H1").Value = "referenciaCabecero";
+            worksheet2.Cell("I1").Value = "textoCabecero";
+            worksheet2.Cell("J1").Value = "llaveSistema";
+            worksheet2.Cell("K1").Value = "primerNumeroReferencia";
+            worksheet2.Cell("L1").Value = "numeroPosicion";
+            worksheet2.Cell("M1").Value = "claveContable";
+            worksheet2.Cell("N1").Value = "numeroCuenta";
+            worksheet2.Cell("O1").Value = "importe";
+            worksheet2.Cell("P1").Value = "codigoCentroCostos";
+            worksheet2.Cell("Q1").Value = "codigoAsignacion";
+            worksheet2.Cell("R1").Value = "texto";
+            worksheet2.Cell("S1").Value = "indicadorIva";
+            worksheet2.Cell("T1").Value = "importeImpuesto";
+            worksheet2.Cell("U1").Value = "codigoDivision";
+            #endregion
+
+            int fila = 1;
+            foreach (var poliza in listadoPoliza)
             {
-                var worksheet = workbook.Worksheets.Add("Hoja 1");
-                worksheet.Cell("A1").Value = "FechaDocumento";
-                worksheet.Cell("B1").Value = "IdSociedad";
-                worksheet.Cell("C1").Value = "ClaseDocumento";
-                worksheet.Cell("D1").Value = "FechaContable";
-                worksheet.Cell("E1").Value = "Periodo";
-                worksheet.Cell("F1").Value = "TipoCambio";
-                worksheet.Cell("G1").Value = "CodigoMoneda";
-                worksheet.Cell("H1").Value = "ReferenciaCabecero";
-                worksheet.Cell("I1").Value = "TextoCabecero";
-                worksheet.Cell("J1").Value = "LlaveSistema";
-                worksheet.Cell("K1").Value = "PrimerNumeroReferencia";
-                worksheet.Cell("L1").Value = "NumeroPosicion";
-                worksheet.Cell("M1").Value = "ClaveContable";
-                worksheet.Cell("N1").Value = "NumeroCuenta";
-                worksheet.Cell("O1").Value = "Importe";
-                worksheet.Cell("P1").Value = "CodigoCentroCostos";
-                worksheet.Cell("Q1").Value = "CodigoAsignacion";
-                worksheet.Cell("R1").Value = "Texto";
-                worksheet.Cell("S1").Value = "IndicadorIva";
-                worksheet.Cell("T1").Value = "ImporteImpuesto";
-                worksheet.Cell("U1").Value = "CodigoDivision";
+                fila++;
+                #region Datos Primer Excel
+                //worksheet1.Cell($"A{fila}").Value = poliza.FolioPoliza;
+                //worksheet1.Cell($"B{fila}").Value = poliza.TipoCambio;
+                //worksheet1.Cell($"C{fila}").Value = poliza.FechaContable;
+                //worksheet1.Cell($"D{fila}").Value = poliza.Importe;
+                //worksheet1.Cell($"E{fila}").Value = poliza.Concepto;
+                //worksheet1.Cell($"F{fila}").Value = poliza.Renglon;
+                //worksheet1.Cell($"G{fila}").Value = poliza.CodigoCuentaContable;
+                //worksheet1.Cell($"H{fila}").Value = poliza.Concepto2;
+                //worksheet1.Cell($"I{fila}").Value = poliza.Cargo;
+                //worksheet1.Cell($"J{fila}").Value = poliza.Abono;
+                //worksheet1.Cell($"K{fila}").Value = poliza.NombreCuentaContable;
+                #endregion
 
-
-
-                int fila = 1;
-                foreach (var poliza in listadoPoliza)
-                {
-                    fila++;
-                    worksheet.Cell($"A{fila}").Value = poliza.FechaContable;
-                    worksheet.Cell($"B{fila}").Value = poliza.IdSociedad;
-                    worksheet.Cell($"C{fila}").Value = poliza.ClaseDocumento;
-                    worksheet.Cell($"D{fila}").Value = poliza.FechaContable;
-                    worksheet.Cell($"E{fila}").Value = poliza.Periodo;
-                    worksheet.Cell($"F{fila}").Value = poliza.TipoCambio;
-                    worksheet.Cell($"G{fila}").Value = poliza.IdMoneda;
-                    worksheet.Cell($"H{fila}").Value = poliza.ReferenciaCabecero;
-                    worksheet.Cell($"I{fila}").Value = poliza.TextoCabecero;
-                    worksheet.Cell($"J{fila}").Value = poliza.LlaveSistema;
-                    worksheet.Cell($"K{fila}").Value = poliza.PrimerNumeroReferencia;
-                    worksheet.Cell($"L{fila}").Value = poliza.NumeroPosicion;
-                    worksheet.Cell($"M{fila}").Value = poliza.ClaveContable;
-                    worksheet.Cell($"N{fila}").Value = poliza.NumeroCuenta;
-                    worksheet.Cell($"O{fila}").Value = poliza.Importe;
-                    worksheet.Cell($"P{fila}").Value = poliza.CentroCostosId;
-                    worksheet.Cell($"Q{fila}").Value = poliza.AsignacionId;
-                    worksheet.Cell($"R{fila}").Value = poliza.Texto;
-                    worksheet.Cell($"S{fila}").Value = poliza.IndicadorIva;
-                    worksheet.Cell($"T{fila}").Value = poliza.ImporteImpuesto;
-                    worksheet.Cell($"U{fila}").Value = poliza.DivisionId;
-                }
-                workbook.SaveAs("C:\\Excels\\Colector-Contable.xlsx");
+                #region Datos Segundo Excel
+                worksheet2.Cell($"A{fila}").Value = poliza.FechaContable;
+                worksheet2.Cell($"B{fila}").Value = poliza.IdSociedad;
+                worksheet2.Cell($"C{fila}").Value = poliza.ClaseDocumento;
+                worksheet2.Cell($"D{fila}").Value = poliza.FechaContable;
+                worksheet2.Cell($"E{fila}").Value = poliza.Periodo;
+                worksheet2.Cell($"F{fila}").Value = poliza.TipoCambio;
+                worksheet2.Cell($"G{fila}").Value = poliza.IdMoneda;
+                worksheet2.Cell($"H{fila}").Value = poliza.ReferenciaCabecero;
+                worksheet2.Cell($"I{fila}").Value = poliza.TextoCabecero;
+                worksheet2.Cell($"J{fila}").Value = poliza.LlaveSistema;
+                worksheet2.Cell($"K{fila}").Value = poliza.PrimerNumeroReferencia;
+                worksheet2.Cell($"L{fila}").Value = poliza.NumeroPosicion;
+                worksheet2.Cell($"M{fila}").Value = poliza.ClaveContable;
+                worksheet2.Cell($"N{fila}").Value = poliza.NumeroCuenta;
+                worksheet2.Cell($"O{fila}").Value = poliza.Importe;
+                worksheet2.Cell($"P{fila}").Value = poliza.CentroCostosId;
+                worksheet2.Cell($"Q{fila}").Value = poliza.AsignacionId;
+                worksheet2.Cell($"R{fila}").Value = poliza.Texto;
+                worksheet2.Cell($"S{fila}").Value = poliza.IndicadorIva;
+                worksheet2.Cell($"T{fila}").Value = poliza.ImporteImpuesto;
+                worksheet2.Cell($"U{fila}").Value = poliza.DivisionId;
+                #endregion
             }
+            woorbook1.SaveAs($"C:\\ServicioColectorContable\\PSListadoPolizas{DateTime.Now.ToString("yyyyMMdd")}.xlsx");
+            workbook2.SaveAs($"C:\\ServicioColectorContable\\PSResumenPolizas{DateTime.Now.ToString("yyyyMMdd")}.xlsx");
         }
-
     }
 }
