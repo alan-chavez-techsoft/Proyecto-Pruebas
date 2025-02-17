9853245d3384dc9fb80cdbf14b1dcd509f9c54c1
Alan Chávez <alan.chavez@techsoft.com.mx>
2025-02-17 15:15:38 -0700

    Se hizo servicio para crear excel

---
+++ CreacionExcel/PolicyBody.cs
@@ -0,0 +1,30 @@
+﻿namespace CreacionExcel
+{
+    public class PolicyBody
+    {
+        public DateTime FechaDocumento { get; set; }//dd.MM.yyyy
+        public string IdSociedad { get; set; } = string.Empty;
+        public string ClaseDocumento { get; set; } = string.Empty;
+        public DateTime FechaContable { get; set; }//yyyy-MM-dd
+        public DateTime Periodo { get; set; }//MM
+        public int? TipoCambio { get; } = null;
+        public string IdMoneda { get; } = "MXM";
+        public int ReferenciaCabecero { get; set; }//Pendiente
+        public string TextoCabecero { get; set; } = string.Empty;//Pendiente
+        public string LlaveSistema { get; set; } = string.Empty;//Confirmar
+        public string PrimerNumeroReferencia { get; set; } = string.Empty;//Confirmar
+
+        //Detalle
+        public int NumeroPosicion { get; set; }
+        public int ClaveContable { get; set; }
+        public int NumeroCuenta { get; set; }
+        public decimal Importe { get; set; }
+        public string CentroCostosId { get; set; } = string.Empty;
+        public string AsignacionId { get; set; } = string.Empty;
+        public string Texto { get; set; } = string.Empty;//PENDIENTE
+        public int IndicadorIva { get; set; }
+        public decimal? ImporteImpuesto { get; set; }
+        public string DivisionId { get; set; } = string.Empty;
+    }
+
+}
---
+++ CreacionExcel/Program.cs
@@ -0,0 +1,103 @@
+﻿using ClosedXML.Excel;
+
+namespace CreacionExcel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = ObtenerLista();
+            GenerarExcel(lista);
+        }
+
+        static List<PolicyBody> ObtenerLista()
+        {
+            var lista = new List<PolicyBody>();
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add(new PolicyBody
+                {
+                    FechaDocumento = DateTime.Now,
+                    IdSociedad = "IdSociedad",
+                    ClaseDocumento = "ClaseDocumento",
+                    FechaContable = DateTime.Now,
+                    Periodo = DateTime.Now,
+                    ReferenciaCabecero = 1,
+                    TextoCabecero = "TextoCabecero",
+                    LlaveSistema = "LlaveSistema",
+                    PrimerNumeroReferencia = "PrimerNumeroReferencia",
+                    NumeroPosicion = 1,
+                    ClaveContable = 1,
+                    NumeroCuenta = 1,
+                    Importe = 1,
+                    CentroCostosId = "CentroCostosId",
+                    AsignacionId = "AsignacionId",
+                    Texto = "Texto",
+                    IndicadorIva = 1,
+                    ImporteImpuesto = 1,
+                    DivisionId = "DivisionId"
+                });
+            }
+            return lista;
+        }
+        static void GenerarExcel(List<PolicyBody> listadoPoliza)
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Hoja 1");
+                worksheet.Cell("A1").Value = "FechaDocumento";
+                worksheet.Cell("B1").Value = "IdSociedad";
+                worksheet.Cell("C1").Value = "ClaseDocumento";
+                worksheet.Cell("D1").Value = "FechaContable";
+                worksheet.Cell("E1").Value = "Periodo";
+                worksheet.Cell("F1").Value = "TipoCambio";
+                worksheet.Cell("G1").Value = "CodigoMoneda";
+                worksheet.Cell("H1").Value = "ReferenciaCabecero";
+                worksheet.Cell("I1").Value = "TextoCabecero";
+                worksheet.Cell("J1").Value = "LlaveSistema";
+                worksheet.Cell("K1").Value = "PrimerNumeroReferencia";
+                worksheet.Cell("L1").Value = "NumeroPosicion";
+                worksheet.Cell("M1").Value = "ClaveContable";
+                worksheet.Cell("N1").Value = "NumeroCuenta";
+                worksheet.Cell("O1").Value = "Importe";
+                worksheet.Cell("P1").Value = "CodigoCentroCostos";
+                worksheet.Cell("Q1").Value = "CodigoAsignacion";
+                worksheet.Cell("R1").Value = "Texto";
+                worksheet.Cell("S1").Value = "IndicadorIva";
+                worksheet.Cell("T1").Value = "ImporteImpuesto";
+                worksheet.Cell("U1").Value = "CodigoDivision";
+
+
+
+                int fila = 1;
+                foreach (var poliza in listadoPoliza)
+                {
+                    fila++;
+                    worksheet.Cell($"A{fila}").Value = poliza.FechaContable;
+                    worksheet.Cell($"B{fila}").Value = poliza.IdSociedad;
+                    worksheet.Cell($"C{fila}").Value = poliza.ClaseDocumento;
+                    worksheet.Cell($"D{fila}").Value = poliza.FechaContable;
+                    worksheet.Cell($"E{fila}").Value = poliza.Periodo;
+                    worksheet.Cell($"F{fila}").Value = poliza.TipoCambio;
+                    worksheet.Cell($"G{fila}").Value = poliza.IdMoneda;
+                    worksheet.Cell($"H{fila}").Value = poliza.ReferenciaCabecero;
+                    worksheet.Cell($"I{fila}").Value = poliza.TextoCabecero;
+                    worksheet.Cell($"J{fila}").Value = poliza.LlaveSistema;
+                    worksheet.Cell($"K{fila}").Value = poliza.PrimerNumeroReferencia;
+                    worksheet.Cell($"L{fila}").Value = poliza.NumeroPosicion;
+                    worksheet.Cell($"M{fila}").Value = poliza.ClaveContable;
+                    worksheet.Cell($"N{fila}").Value = poliza.NumeroCuenta;
+                    worksheet.Cell($"O{fila}").Value = poliza.Importe;
+                    worksheet.Cell($"P{fila}").Value = poliza.CentroCostosId;
+                    worksheet.Cell($"Q{fila}").Value = poliza.AsignacionId;
+                    worksheet.Cell($"R{fila}").Value = poliza.Texto;
+                    worksheet.Cell($"S{fila}").Value = poliza.IndicadorIva;
+                    worksheet.Cell($"T{fila}").Value = poliza.ImporteImpuesto;
+                    worksheet.Cell($"U{fila}").Value = poliza.DivisionId;
+                }
+                workbook.SaveAs("C:\\Excels\\Colector-Contable.xlsx");
+            }
+        }
+
+    }
+}
